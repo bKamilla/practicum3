da63e0c3ae4104ebb887f60caa5894263b82f59b
bKamilla <k.batrakova@g.nsu.ru>
2024-03-01 22:07:04 +0700

    2

--- 2.3.py
+++ 2.3.py
@@ -1,1 +1,9 @@
 #practicum3
+
+seconds = int(input("Введите время в секундах:"))
+hours = seconds // 3600
+A = seconds % 3600
+minutes = A // 60
+sec = A % 60
+print("Текущее время:",hours,'часов',minutes,'минут',sec,'секунд')
+
