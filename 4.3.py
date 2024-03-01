daa493bf34408a88f88dfb50746973e12b44e236
bKamilla <k.batrakova@g.nsu.ru>
2024-03-01 22:19:00 +0700

    2

--- 4.3.py
+++ 4.3.py
@@ -1,1 +1,8 @@
 #practicum3
+
+X,Y,N = map(int, input("Введите через пробел рубли,копейки,кол-во заказов: ").split())
+cop = X*100 + Y
+income = cop*N
+rub = income // 100
+cop2 = income % 100
+print("Доход за день:",rub,'руб.',cop2,'коп.')
